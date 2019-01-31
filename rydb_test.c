30ccbdf8d86546876f617ed545d1826de29a7698
Leo P <junk@slact.net>
2019-01-31 17:59:08 -0500

    filin'

--- rydb_test.c
+++ rydb_test.c
@@ -12,7 +12,7 @@ int main(void) {
   rydb_config_add_index_hashtable(db, "zebra", 1, 4, 0, NULL);
   rydb_config_add_index_hashtable(db, "beepus", 4, 11, 1, NULL);
   rydb_config_add_index_hashtable(db, "anthill", 45, 4, 0, NULL);
-  if(rydb_open(db, "./" ,"foo")) {
+  if(rydb_open(db, "db/" ,"foo")) {
     printf("opened ok\n");
     rydb_close(db);
     return 1;
