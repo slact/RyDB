f94f44092553599e60d069cec15a7db173265d18
Leo P <junk@slact.net>
2019-03-17 19:42:55 -0400

    eeeh okay..

--- .configure-custom.sh
+++ .configure-custom.sh
@@ -3,3 +3,6 @@ ENABLE_VARS="$ENABLE_VARS rydb-debug|yes|RYDB_DEBUG"
 DISABLE_VARS="$DISABLE_VARS rydb-debug|0|RYDB_DEBUG"
 ENABLE_RYDB_DEBUG_DOC="enable RYDB_DEBUG code for development & debugging"
 DISABLE_RYDB_DEBUG_DOC="disable RYDB_DEBUG code for development & debugging"
+
+WITH_VARS="$WITH_VARS build-type|Debug|CMAKE_BUILD_TYPE"
+WITH_CMAKE_BUILD_TYPE_DOC="cmake build type (Debug,Release,RelWithDebInfo)"
